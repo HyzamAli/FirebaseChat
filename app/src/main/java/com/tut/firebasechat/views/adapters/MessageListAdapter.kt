7e76387c87f6d151010c1e3783d08a60d33af8dc
hyzam <hyzamzz@gmail.com>
2021-05-29 21:52:22 +0530

    UI layer changes for Fix #16

--- app/src/main/java/com/tut/firebasechat/views/adapters/MessageListAdapter.kt
+++ app/src/main/java/com/tut/firebasechat/views/adapters/MessageListAdapter.kt
@@ -3,26 +3,31 @@ package com.tut.firebasechat.views.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.tut.firebasechat.databinding.RowMessageRecievedBinding
 import com.tut.firebasechat.databinding.RowMessageSentBinding
 import com.tut.firebasechat.models.Message
 
-class MessageListAdapter : PagingDataAdapter<Message, MessageListAdapter.ViewHolder>(MessageDiffCallBack()) {
 
-    private val viewTypeSender = 33
+const val VIEW_TYPE_SENDER = 33
 
-    private val viewTypeReceiver = 34
+const val VIEW_TYPE_RECEIVER = 34
+
+class ViewHolder(val bindingSender: RowMessageSentBinding? = null,
+                 val bindingReceiver: RowMessageRecievedBinding? = null)
+    : RecyclerView.ViewHolder(bindingSender?.root?:bindingReceiver!!.root)
+
+class MessageListAdapter : PagingDataAdapter<Message, ViewHolder>(Message()) {
 
     override fun getItemViewType(position: Int): Int {
-        return if (getItem(position)?.sender == FirebaseAuth.getInstance().uid) viewTypeSender
-        else viewTypeReceiver
+        return if (getItem(position)?.sender == FirebaseAuth.getInstance().uid) VIEW_TYPE_SENDER
+        else VIEW_TYPE_RECEIVER
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return if (viewType == viewTypeSender) {
+        return if (viewType == VIEW_TYPE_SENDER) {
             val binding: RowMessageSentBinding =
                     RowMessageSentBinding.inflate(LayoutInflater.from(parent.context),
                             parent,
@@ -45,19 +50,37 @@ class MessageListAdapter : PagingDataAdapter<Message, MessageListAdapter.ViewHol
             holder.bindingReceiver.setMessage(getItem(position))
         }
     }
+}
 
-    class ViewHolder(val bindingSender: RowMessageSentBinding? = null,
-                     val bindingReceiver: RowMessageRecievedBinding? = null)
-        : RecyclerView.ViewHolder(bindingSender?.root?:bindingReceiver!!.root)
+class LiveMessageListAdapter() : ListAdapter<Message, ViewHolder>(Message()) {
 
-    class MessageDiffCallBack : DiffUtil.ItemCallback<Message>() {
-        override fun areItemsTheSame(oldItem: Message, newItem: Message): Boolean {
-            return oldItem.docId == newItem.docId
+    override fun getItemViewType(position: Int): Int {
+        return if (getItem(position)?.sender == FirebaseAuth.getInstance().uid) VIEW_TYPE_SENDER
+        else VIEW_TYPE_RECEIVER
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return if (viewType == VIEW_TYPE_SENDER) {
+            val binding: RowMessageSentBinding =
+                    RowMessageSentBinding.inflate(LayoutInflater.from(parent.context),
+                            parent,
+                            false)
+            ViewHolder(bindingSender = binding)
+        } else {
+            val binding: RowMessageRecievedBinding =
+                    RowMessageRecievedBinding.inflate(LayoutInflater.from(parent.context),
+                            parent,
+                            false)
+            ViewHolder(bindingReceiver = binding)
         }
+    }
 
-        override fun areContentsTheSame(oldItem: Message, newItem: Message): Boolean {
-            return oldItem == newItem
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindingSender?.let {
+            holder.bindingSender.setMessage(getItem(position))
         }
-
+        holder.bindingReceiver?.let {
+            holder.bindingReceiver.setMessage(getItem(position))
+        }
     }
 }
--- app/src/main/java/com/tut/firebasechat/views/fragments/MessageFragment.kt
+++ app/src/main/java/com/tut/firebasechat/views/fragments/MessageFragment.kt
@@ -9,11 +9,18 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.tut.firebasechat.databinding.FragmentMessageBinding
+import com.tut.firebasechat.models.FirebaseResponse
+import com.tut.firebasechat.models.Message
 import com.tut.firebasechat.viewmodels.MessageViewModel
+import com.tut.firebasechat.views.adapters.LiveMessageListAdapter
 import com.tut.firebasechat.views.adapters.MessageListAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MessageFragment : Fragment() {
 
@@ -21,6 +28,9 @@ class MessageFragment : Fragment() {
     private lateinit var viewModel: MessageViewModel
     private val args: MessageFragmentArgs by navArgs()
     private lateinit var adapter: MessageListAdapter
+    private lateinit var liveMessageAdapter: LiveMessageListAdapter
+    private var jobGetPrevMessages: Job? = null
+    private var jobGetLiveMessages: Job? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,14 +43,48 @@ class MessageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         adapter = MessageListAdapter()
+        liveMessageAdapter = LiveMessageListAdapter()
         binding.messagesList.adapter = adapter
+        binding.liveMessagesList.adapter = liveMessageAdapter
         getPreviousMessages()
     }
 
-    private fun getPreviousMessages() = lifecycleScope.launch {
-        viewModel.getPrevMessages(args.messageId).distinctUntilChanged()
-                .collectLatest {
-                    adapter.submitData(it)
-                }
+    private fun getPreviousMessages() {
+        var firstCollectionData = true
+        jobGetPrevMessages = lifecycleScope.launch {
+            viewModel.getPrevMessages(args.messageId).distinctUntilChanged()
+                    .collectLatest {
+                        if (firstCollectionData) getLiveMessageStream().also {
+                            firstCollectionData = false
+                        }
+                        adapter.submitData(it)
+                    }
+        }
+    }
+
+    private fun getLiveMessageStream() {
+        jobGetLiveMessages = lifecycleScope.launch {
+            viewModel.getLiveMessageStream(args.messageId).distinctUntilChanged()
+                    .collect { responseWrapper ->
+                        if (responseWrapper.response == FirebaseResponse.SUCCESS) {
+                            responseWrapper.data?.let {
+                                if (it.isNotEmpty()) {
+                                    val list: MutableList<Message> = it.toMutableList()
+                                    list.addAll(liveMessageAdapter.currentList)
+                                    withContext(Dispatchers.Main) {
+                                        liveMessageAdapter.submitList(list.toList())
+                                    }
+                                }
+                            }
+                        } // TODO: handle exception
+                    }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        jobGetPrevMessages?.let { if(it.isActive) it.cancel() }
+        jobGetLiveMessages?.let { if(it.isActive) it.cancel() }
+        super.onDestroyView()
     }
 }
