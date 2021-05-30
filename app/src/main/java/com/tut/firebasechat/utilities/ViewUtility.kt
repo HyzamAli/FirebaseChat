7be27f0875f17cb5c362cf9f44f45ba792257688
hyzam <hyzamzz@gmail.com>
2021-05-30 18:09:49 +0530

    Updated Resources

--- app/src/main/java/com/tut/firebasechat/utilities/ViewUtility.kt
+++ app/src/main/java/com/tut/firebasechat/utilities/ViewUtility.kt
@@ -1,12 +1,22 @@
 package com.tut.firebasechat.utilities
 
 import android.app.Activity
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.google.android.material.snackbar.Snackbar
 
 object ViewUtility {
     fun showSnack(activity: Activity, message:String, duration: Int = Snackbar.LENGTH_SHORT) {
         val rootView: View = activity.window.decorView.findViewById(android.R.id.content)
         Snackbar.make(rootView, message, duration).show()
+    }
+
+    fun hideKeyboard(activity: Activity) {
+        val view = activity.findViewById<View>(android.R.id.content)
+        if (view != null) {
+            val imm: InputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
