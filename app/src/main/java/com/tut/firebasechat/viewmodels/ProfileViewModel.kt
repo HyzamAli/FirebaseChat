cb94227361423ece9efe1d335f813b970c923133
hyzam <hyzamzz@gmail.com>
2021-06-16 23:41:50 +0530

    refactored registration system code

--- app/src/main/java/com/tut/firebasechat/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/com/tut/firebasechat/viewmodels/ProfileViewModel.kt
@@ -8,21 +8,19 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.tut.firebasechat.R
 import com.tut.firebasechat.models.FirebaseResponse
-import com.tut.firebasechat.models.ResponseWrapper
 import com.tut.firebasechat.models.User
 import com.tut.firebasechat.repositories.AuthRepository
 import com.tut.firebasechat.repositories.ProfileRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import timber.log.Timber
 
 class ProfileViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = ProfileRepository
     private var user: User? = null
     private val app = getApplication<Application>()
     val regResponse: MutableLiveData<FirebaseResponse> = MutableLiveData()
+    private val defaultDispatcher = Dispatchers.IO
 
     fun putProfileDetails(name: String, username: String, imgUri: Uri?) = viewModelScope.launch {
         val checkUsernameExistsResponse = repository.checkUsernameExists(username)
@@ -52,7 +50,7 @@ class ProfileViewModel(application: Application) : AndroidViewModel(application)
                         val putDetailsResponse = repository.putProfileDetails(user)
 
                         if (putDetailsResponse ==  FirebaseResponse.SUCCESS) {
-                            putProfileCompleted()
+                            putProfileCompleted(tokenAdded = true)
                         }
                         regResponse.postValue(putDetailsResponse)
                     }
@@ -72,14 +70,10 @@ class ProfileViewModel(application: Application) : AndroidViewModel(application)
                 .cachedIn(viewModelScope)
     }
 
-    fun isProfileExists(): LiveData<ResponseWrapper<Boolean>> {
-        val response: MediatorLiveData<ResponseWrapper<Boolean>> = MediatorLiveData()
-        response.addSource(repository.isProfileExists()) { responseWrapper ->
-            if (responseWrapper.response == FirebaseResponse.SUCCESS &&
-                responseWrapper.data == true) putProfileCompleted()
-            response.value = responseWrapper
-        }
-        return response
+    fun isProfileExists() = liveData(defaultDispatcher) {
+        val response = repository.isProfileExists()
+        if (response.response == FirebaseResponse.SUCCESS && response.data!!) putProfileCompleted()
+        emit(response)
     }
 
     fun putFcmToken() {
@@ -91,7 +85,7 @@ class ProfileViewModel(application: Application) : AndroidViewModel(application)
             viewModelScope.launch {
                 val response = repository.putFcmToken(fcmToken!!)
                 if (response == FirebaseResponse.SUCCESS) {
-                    putProfileCompleted()
+                    putProfileCompleted(tokenAdded = true)
                 }
             }
         }
@@ -100,10 +94,10 @@ class ProfileViewModel(application: Application) : AndroidViewModel(application)
     fun isProfileCompleted(): Boolean =
             getStore().getBoolean(app.getString(R.string.KEY_PROFILE_UPDATED), false)
 
-    private fun putProfileCompleted() =
+    private fun putProfileCompleted(tokenAdded: Boolean = false) =
             getStore().edit().apply {
                 this.putBoolean(app.getString(R.string.KEY_PROFILE_UPDATED), true)
-                this.putBoolean(app.getString(R.string.KEY_FCM_UPDATED), true)
+                this.putBoolean(app.getString(R.string.KEY_FCM_UPDATED), tokenAdded)
                 this.apply()
             }
 
