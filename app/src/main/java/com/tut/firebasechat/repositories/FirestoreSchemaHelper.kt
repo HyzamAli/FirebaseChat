d9785b99d7959682030cfbf4cdd9fe508222cc2e
hyzam <hyzamzz@gmail.com>
2021-06-18 19:51:49 +0530

    Added FirestoreSchemaHelper.kt

---
+++ app/src/main/java/com/tut/firebasechat/repositories/FirestoreSchemaHelper.kt
@@ -0,0 +1,21 @@
+package com.tut.firebasechat.repositories
+
+// Path -> /
+const val USER_COLLECTIONS = "Users"
+
+// Path ->/
+// Path -> /Users/{userDocument}/
+const val CHAT_COLLECTIONS = "Chats"
+
+// Path -> /Users/{userDocument}/Chats/{chatDocument}
+// Path -> /Chats/{chatDocument}/Messages/{messageDocument}
+const val TIME_STAMP_FIELD = "time_stamp"
+
+// Path -> /Users/{userDocument}
+const val USERNAME_FIELD = "username"
+
+// Path -> /Users/{userDocument}/Chats/{chatDocument}
+const val PARTY_ONE_FIELD = "party_one"
+
+// Path -> /Users/{userDocument}/Chats/{chatDocument}
+const val PARTY_TWO_FIELD = "party_two"
