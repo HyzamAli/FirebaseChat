c5c9c2d0129b8d948187517400ae0ff338479103
hyzam <hyzamzz@gmail.com>
2021-06-20 20:32:57 +0530

    Refactored Auth View Code

--- app/src/main/java/com/tut/firebasechat/views/fragments/VerifyOtpFragment.kt
+++ app/src/main/java/com/tut/firebasechat/views/fragments/VerifyOtpFragment.kt
@@ -2,6 +2,8 @@ package com.tut.firebasechat.views.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.tut.firebasechat.R
 import com.tut.firebasechat.databinding.FragmentVerifyOtpBinding
 import com.tut.firebasechat.models.FirebaseResponse
-import com.tut.firebasechat.utilities.ViewUtility.showSnack
+import com.tut.firebasechat.utilities.ViewUtility
 import com.tut.firebasechat.viewmodels.AuthViewModel
 import com.tut.firebasechat.views.activities.RegistrationActivity
 
@@ -31,23 +33,40 @@ class VerifyOtpFragment : BaseFragment() {
         binding.buttonSubmit.setOnClickListener {
             validateAndRequest()
         }
+        binding.otpField.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.otpContainer.error = null
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     private fun validateAndRequest() {
         val code = binding.otpField.text.toString()
         if (code.length<6) {
-            showSnack(requireActivity(), getString(R.string.text_invalid_phone))
+            binding.otpContainer.error = getString(R.string.text_invalid_phone)
             return
         }
-        requestOtp(code)
+        viewModel.otpCode = code
+        verifyOtp()
     }
 
-    private fun requestOtp(code: String) {
+    private fun verifyOtp() {
+        showLoadingUI()
         binding.buttonSubmit.isEnabled = false
-        viewModel.verifyOTP(code).observe(viewLifecycleOwner) { response ->
+        viewModel.verifyOTP().observe(viewLifecycleOwner) { response ->
+            hideLoadingUI()
             binding.buttonSubmit.isEnabled = true
-            if (response ==  FirebaseResponse.SUCCESS) intentRegistration()
-            else handleError(response)
+            when (response) {
+                FirebaseResponse.SUCCESS -> intentRegistration()
+                FirebaseResponse.INVALID_CREDENTIALS -> {
+                    ViewUtility.showSnack(requireActivity(), getString(R.string.text_invalid_otp))
+                }
+                else -> handleError(response){verifyOtp()}
+            }
         }
     }
 
@@ -56,5 +75,16 @@ class VerifyOtpFragment : BaseFragment() {
             startActivity(this)
             requireActivity().finish()
         }
+    }
+
+    override fun showLoadingUI() {
+        super.showLoadingUI()
+        binding.progressbar.show()
+    }
+
+    override fun hideLoadingUI(): Boolean {
+        val shouldHide = super.hideLoadingUI()
+        if (shouldHide) binding.progressbar.hide()
+        return shouldHide
     }
 }
